ac1b85d460c86c746ee45a97b81b1eaa4d845b58
Annuai <annuai.annu@gmail.com>
2023-01-10 12:24:35 +0530

    CSS for word break

--- slices/Text/index.js
+++ slices/Text/index.js
@@ -7,7 +7,7 @@ const Text = ({ slice }) => {
   return (
     <Bounded as="section">
       {prismicH.isFilled.richText(slice.primary.text) && (
-        <div className="font-sans leading-relaxed md:text-md md:leading-relaxed">
+        <div className="font-sans leading-relaxed break-words md:text-md md:leading-relaxed">
           <PrismicRichText field={slice.primary.text} />
         </div>
       )}
