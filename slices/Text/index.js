02b9041518591fbdcb0e9bc666992cd8f5921ebf
Annuai <annuai.annu@gmail.com>
2023-01-19 19:12:37 +0530

    Update index.js

--- slices/Text/index.js
+++ slices/Text/index.js
@@ -5,10 +5,13 @@ import { Bounded } from "../../components/Bounded";
 
 const Text = ({ slice }) => {
   return (
-    <Bounded as="section">
+    <Bounded as="section" className="mb-12 first:mt-0 last:mb-0">
       {prismicH.isFilled.richText(slice.primary.text) && (
         <div className="font-sans leading-relaxed break-words md:text-md md:leading-relaxed">
-          <PrismicRichText field={slice.primary.text} />
+          <PrismicRichText field={slice.primary.text}
+          components={{
+            paragraph: ({ children }) => <p className="mt-4">{children}</p> }}
+             />
         </div>
       )}
     </Bounded>
